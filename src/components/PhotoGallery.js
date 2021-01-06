3f6d17a85fb3fd34843bf2dc74625dc62dc1eb9b
PragmaticWays <adam.allard@live.com>
2021-01-05 21:44:42 -0500

    adding material icons nav before and nav next

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { MdNavigateBefore, MdNavigateNext } from 'react-icons/md';
 
 export default function PhotoGallery({ photos }) {
+  const prevPhoto = () => {
+    console.log('previous photo');
+  };
+
+  const nextPhoto = () => {
+    console.log('next photo');
+  };
+
   return (
     <>
       {photos.map((photo, index) => {
         return (
           <div key={index}>
             <img src={photo.url} alt={photo.caption} />
+            <MdNavigateBefore onClick={prevPhoto} />
+            <MdNavigateNext onClick={nextPhoto} />
             <p>{photo.caption}</p>
           </div>
         );
